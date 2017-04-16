1873c7c568474008b03d354f1d50ea5578804bf4
Joe Essey <joe.essey@accesscfa.com>
2017-04-16 17:07:47 -0400

    tracking win state and player turn in 1 player

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -3,11 +3,17 @@ class PointsController < ApplicationController
 	def update
 		@point = Point.find(params[:id])
 		@point.update(point_params)
-		#this is a hack, should conditionally set state on update by checking games player id and player number (1 or 2)
-		
 		@point.player_1! if params[:player_id].present? && Player.find(params[:player_id]) == @point.board.game.player_1
 		@point.player_2! if params[:player_id].present? && Player.find(params[:player_id]) == @point.board.game.player_2
 
+		board_analyzer = BoardAnalyzer.new(@point.board, @point, @point.player)
+    	if board_analyzer.winning_position?
+			@player = @point.player
+			respond_to do |format|
+	        	format.js { render template: "boards/show_winner" }
+	      	end
+		end
+	
 		@new_valid_point = Point.new_valid_point(@point) if @point.y_coord < 5
 	end
 
